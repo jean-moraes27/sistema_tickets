94c97bf573bf03285f45c9319ff7b3689de0eaff
Breno Riot <brenopsilva95@hotmail.com>
2021-08-02 20:17:32 -0300

    { ajustes } adicionando icone e favicon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!([{
     name: "Administrador Ticket",
     email: "admin@ticket.com",
@@ -17,4 +10,4 @@ User.create!([{
     password: 'usuario'
 }])
 
-p "Created #{User.count} usuários"
+p "#{User.count} usuários cadastrados"
