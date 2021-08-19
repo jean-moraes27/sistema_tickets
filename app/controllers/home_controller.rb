bafb5bbc7a8b0d2c32750a820feddca1ef69d745
Jean Moraes <jeancmpereira1993@gmail.com>
2021-08-19 17:27:48 -0300

    Ajustado contagem de tickets na index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,8 @@ class HomeController < ApplicationController
   before_action :authorize
   add_breadcrumb 'Inicio', :root_path
   def index
-    @tickets_opened = Ticket.where(['tipo = ? and status = ?', 1, 0]).count
-    @tickets_closed = Ticket.where(['tipo = ? and status = ?', 2, 0]).count
-    @tickets_attend = Ticket.where(['tipo = ? and status = ?', 3, 0]).count
+    @tickets_opened = Ticket.where(['status in (?)', [0, 1]]).count
+    @tickets_attend = Ticket.where(['status = ?', 1]).count
+    @tickets_closed = Ticket.where(['status = ?', 2]).count
   end
 end
