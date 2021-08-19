d6ec33c18217e32f8956360351eb7d1bc26de79a
Breno Riot <brenopsilva95@hotmail.com>
2021-08-19 14:03:15 -0300

    { ajustes } paginas alteradas para slim e criado relacionamento de ticket-user

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ApplicationRecord
     validates :title, presence: true, presence:{ message: 'Informe o título do chamado' }
     validates :content, presence: true, presence:{ message: 'Informe o conteúdo do chamado' }
+
+    has_one :atendente, :class_name => "User",  :foreign_key => 'id', :primary_key => 'attendant'
 end
