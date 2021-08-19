0d2591df010d3c280ed15a78e8a4f220daf18905
Breno Riot <brenopsilva95@hotmail.com>
2021-08-19 18:01:39 -0300

    { ajustes } melhorando view de show do ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,10 +2,10 @@
 
 # model de ticket
 class Ticket < ApplicationRecord
-  validates :title, presence: true, presence: { message: 'Informe o título do chamado' }
-  validates :tipo, presence: true, presence: { message: 'Informe o tipo do chamado' }
-  validates :attendant, presence: true, presence: { message: 'Informe o responsável pelo chamado' }
-  validates :content, presence: true, presence: { message: 'Informe o conteúdo do chamado' }
+  validates :title, presence: { message: 'Informe o título do chamado' }
+  validates :tipo, presence: { message: 'Informe o tipo do chamado' }
+  validates :attendant, presence: { message: 'Informe o responsável pelo chamado' }
+  validates :content, presence: { message: 'Informe o conteúdo do chamado' }
 
   has_one :atendente, class_name: 'User', foreign_key: 'id', primary_key: 'attendant'
 end
